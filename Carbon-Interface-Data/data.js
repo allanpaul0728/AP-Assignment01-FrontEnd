2587d74cb34179eeda1ff6f0b3b87c4f164cc554
allanpaul0728 <allan.paul28@yahoo.com>
2022-06-12 14:41:56 +0000

    Added code on script and data

---
+++ Carbon-Interface-Data/data.js
@@ -0,0 +1,13 @@
+const response = await fetch('https://www.carboninterface.com/api/v1/estimates', {
+  method: 'POST',
+  headers: {
+    'Authorization': `Bearer ${appKey}`,
+    'Content-Type': 'application/json'
+},
+  body: JSON.stringify({
+    type: "electricity",
+    electricity_unit: unit,
+    electricity_value: value,
+    country: country
+  })
+});
--- Carbon-Interface-Data/script.js
+++ Carbon-Interface-Data/script.js
@@ -1,6 +1,35 @@
-const DATA_URL = "https://carbon-interface.s3.ca-central-1.amazonaws.com/assets/Carbon+Interface.postman_collection.json";
-const API_KEY = "wwsvjcS8KK1dgtH5euuBPw";
-async function loadData() {
-    let response = await axios.get(DATA_URL);
-    return response.data;
+// const DATA_URL = "https://carbon-interface.s3.ca-central-1.amazonaws.com/assets/Carbon+Interface.postman_collection.json";
+// const API_KEY = "wwsvjcS8KK1dgtH5euuBPw";
+// async function loadData() {
+//     let response = await axios.get(DATA_URL);
+//     return response.data;
+// }
+
+let options = {
+    'chart': {
+        'type':'pie',
+        'height': '100%'
+    },
+    'series': [],
+    'noData':{
+        'text':'Loading'
+    }
 }
+
+let chart = new ApexCharts(
+    document.querySelector('#chart'),
+    options
+);
+
+chart.render();
+
+window.addEventListener('DOMContentLoaded', async function(){
+    let data = await loadData();
+    let series = transformData(data);
+    chart.updateSeries([
+        {
+            'name':'Revenue',
+            'data': series
+        }
+    ])
+}) 
