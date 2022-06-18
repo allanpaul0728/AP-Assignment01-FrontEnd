5b4cf0c0a2a712ad7d14a714d68a12fbeb6e621a
allanpaul0728 <allan.paul28@yahoo.com>
2022-06-18 05:59:58 +0000

    added launch-data

--- Carbon-Interface-Data/script.js
+++ Carbon-Interface-Data/script.js
@@ -1,10 +1,3 @@
-// const DATA_URL = "https://carbon-interface.s3.ca-central-1.amazonaws.com/assets/Carbon+Interface.postman_collection.json";
-// const API_KEY = "wwsvjcS8KK1dgtH5euuBPw";
-// async function loadData() {
-//     let response = await axios.get(DATA_URL);
-//     return response.data;
-// }
-
 let options = {
     'chart': {
         'type':'pie',
